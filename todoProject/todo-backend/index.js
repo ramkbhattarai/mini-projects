36f5edea7ef702136d3f04ee32679d006adb2781
Ram <ramkb466@gmail.com>
2020-06-30 08:35:47 -0400

    added mongodb db

--- todoProject/todo-backend/index.js
+++ todoProject/todo-backend/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
+
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/todos',{useUnifiedTopology: true}, function (err, client) {
+    if (err) throw err
+
+    const db = client.db('todos')
+})
 app.get('/',(req, res)=>{
     res.json('did this work');
+});
+
+app.get('/todos', (req, res) => {
+    const todos = await db.collection('todos').find().toArray();
+    res.json(todos);
 });
 app.listen(3001,()=>{
     console.log('please work');
