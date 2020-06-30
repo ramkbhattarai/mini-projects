f85197f0e5feb8d8dd74d6103abe64394495ecf2
Ram <ramkb466@gmail.com>
2020-06-30 08:19:20 -0400

    added entry file and started to set up request and response

---
+++ todoProject/todo-backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.json('did this work');
+});
+app.listen(3001,()=>{
+    console.log('please work');
+});
