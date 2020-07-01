fb7d3503d78b05f2df9010bc2dc3c408878b3687
Ram <ramkb466@gmail.com>
2020-07-01 09:22:35 -0400

    fixed cors issue

--- todoProject/todo-frontend/src/App.js
+++ todoProject/todo-frontend/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <List/>
     </div>
   );
 }
+const list = ()=> {
+  const [todos, setTodos] = useState([]);
 
+  const fetchTodos = async ()=>{
+   const res= await fetch("http://localhost:3001/todos");
+   console.log(res.json())
+  }
+
+  useEffect(()=>{
+    fetchTodos();
+  }, []);
+
+  return <div>
+    
+  </div>
+};
 export default App;
