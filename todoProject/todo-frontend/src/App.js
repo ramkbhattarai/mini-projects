77c8ce967afcb7aa30295fec9ede89b616337fae
Ram <ramkb466@gmail.com>
2020-07-02 08:20:48 -0400

    request to add todos

--- todoProject/todo-frontend/src/App.js
+++ todoProject/todo-frontend/src/App.js
@@ -10,10 +10,23 @@ function App() {
 }
 const list = ()=> {
   const [todos, setTodos] = useState([]);
+  const [text, setText] = useState([]);
+
 
   const fetchTodos = async ()=>{
    const res= await fetch("http://localhost:3001/todos");
    setTodos(await res.json());
+  }
+
+  const addTodo = async () => {
+    const res = await fetch("http://localhost:3001/todos", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({description: text, done: false})
+
+    });
   }
 
   useEffect(()=>{
@@ -22,14 +35,34 @@ const list = ()=> {
 
   const items = todos.map(todo => <Item todo={todo} key={todo._id} />)
 
-  return <div>
-    {items}
+ 
+  return  <div className="ui card">
+    <div className="content">
+      <div className="header">Todo List Fast</div>
+    </div>
+    <div className="content">
+      <div className="ui relaxed divided list">
+        {items}
+      </div>
+      
+    </div>
+    <div className="extra content">
+      <div className="fluid ui action input">
+        <input type="text" value={text} onChange={(e)=> setText(e.target.value)}/>
+          <button className="ui teal right labeled icon button" onClick={addTodo}>
+            <i className="plus icon"></i>
+            Add
+          </button>
+      </div>
+    </div>
   </div>
+  
+  
 };
 
 const Item = (props)=>{
   const {done, description} = props.todo;
-  return <div>
+  return <div className="item">
     {description}
   </div>
 }
